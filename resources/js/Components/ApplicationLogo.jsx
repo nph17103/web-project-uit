70d080e7a81b878456c4d47fdc5282b48e7edf21
phuochuy <nph17103@gmail.com>
2023-10-06 23:33:22 +0700

    First commit

---
+++ resources/js/Components/ApplicationLogo.jsx
@@ -0,0 +1,10 @@
+export default function ApplicationLogo(props) {
+    return (
+        <div
+            {...props}
+            className="font-monoton text-3xl hover:text-red-800 cursor-pointer text-center transition"
+        >
+            eyesome
+        </div>
+    );
+}
